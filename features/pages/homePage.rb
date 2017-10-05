48ac57311423a529ecc153ef4938398937f26021
Rodrigo Costa <automacao.infracommerce@gmail.com>
2017-10-05 14:55:20 -0300

    Changing xpath to id

--- features/pages/homePage.rb
+++ features/pages/homePage.rb
@@ -1,6 +1,6 @@
 class Home < SitePrism::Page
     element :logoImg, :xpath, '//*[@id="home"]/header/div[2]/div/h1/img'
-    element :mainContent, :xpath, '//*[@id="main-content"]'
+    element :mainContent, '#main-content'
 
     def validateHome
       assert_text('Compra Unilever')
