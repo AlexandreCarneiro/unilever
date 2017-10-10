87c0409181da58c300ae96f9e2aea091ac5d15d8
unknown <automacao.infracommerce@gmail.com>
2017-10-10 17:46:08 -0300

    Commit de Novo Logon

--- features/pages/loginPage.rb
+++ features/pages/loginPage.rb
@@ -1,5 +1,6 @@
 class Login < SitePrism::Page
-    element :registered, '#registered'
+    element :registered, :xpath,'//*[@title="Login/Cadastre-se"]'
+    element :jaCadastrado, :xpath,'//*[@id="registered"]'
     element :user, :xpath, '//*[@id="login"]/form/dl[1]/dd/input'
     element :password, :xpath, '//*[@id="login"]/form/dl[2]/dd/input'
     element :entryButton, :xpath, '//*[@id="login"]/form/p/input'
@@ -7,6 +8,13 @@ class Login < SitePrism::Page
     element :newAccount, :css, '#login > p > a.link.new-account'
     element :errorPassAcc, :xpath,'//*[@id="login"]/form/dl[2]/dd/span'
     
+    def acessLoginPopup
+        registered.click
+    end
+
+    def acesarJaCadastrado
+        jaCadastrado.click
+    end
 
     def doLogin(cnpj, pass)
         user.set cnpj
