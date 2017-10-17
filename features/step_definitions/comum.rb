d22c9dfcc2ca3d299bcda45a7710ad7e7cf8b59e
AlexandreCarneiro <automacao.infracommerce@gmail.com>
2017-10-17 08:49:37 -0200

    Antes do Merge com Login

--- features/step_definitions/comum.rb
+++ features/step_definitions/comum.rb
@@ -1,4 +1,7 @@
 Dado(/^que eu esteja na home do site Unilever$/) do
    @app = App.new
    visit "https://#{'unilever'}:#{'Un!lyiam'}@#{'www-unilever-hcp.infracommerce.com.br'}"
+   if @app.login.btnVerCatalogo.visible?
+   		@app.login.btnVerCatalogo.click
+   end
 end
